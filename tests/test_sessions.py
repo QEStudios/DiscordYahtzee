5bd9c61814d9dbf4277d3c09f86f77958e58185b
SKM GEEK <qestudios17@gmail.com>
2025-04-23 21:32:49 +1000

    Modify tests

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -4,10 +4,34 @@ import yahtzee
 def test_session_creation():
     session = yahtzee.Session()
 
+    assert isinstance(session, yahtzee.Session)
+
+
+def test_session_creation_with_existing_matches():
+    players = [
+        yahtzee.Player("alice", 0),
+        yahtzee.Player("bob", 1),
+        yahtzee.Player("charlie", 2),
+    ]
+
+    matches = [
+        yahtzee.Match(players=players, game_num=0),
+        yahtzee.Match(players=players, game_num=1),
+        yahtzee.Match(players=players, game_num=2),
+    ]
+
+    session = yahtzee.Session(matches=matches)
+
+    assert session.matches == matches
+
+
+def test_session_start_match():
+    session = yahtzee.Session()
+
     alice = yahtzee.Player("alice", 0)
     bob = yahtzee.Player("bob", 1)
     charlie = yahtzee.Player("charlie", 2)
 
     match = session.start_match(players=[alice, bob, charlie])
 
-    print(match)
+    assert isinstance(match, yahtzee.Match)
